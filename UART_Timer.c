222e2aeb54b993b5daa13a50963b6935f1469017
iprince10 <princejha1456@gmail.com>
2026-08-10 12:23:55 +0530

    uart timer file added

---
+++ UART_Timer.c
@@ -0,0 +1,127 @@
+#include <stdint.h>
+
+#define IO_BANK0_BASE 0x40014000u
+
+#define GPIO0_CTRL (*(volatile uint32_t *)(IO_BANK0_BASE + 0x004))
+#define UART0_FUNC 2u
+#define UART0_BASE 0x40034000u
+#define UART0_DR (*(volatile uint32_t *)(UART0_BASE + 0x000))
+#define UART0_FR (*(volatile uint32_t *)(UART0_BASE + 0x018))
+#define UART0_FR_TXFF (1u << 5)
+#define UART0_IBRD (*(volatile uint32_t *)(UART0_BASE + 0x024))
+#define UART0_FBRD (*(volatile uint32_t *)(UART0_BASE + 0x028))
+#define UART0_CR (*(volatile uint32_t *)(UART0_BASE + 0x030))
+#define UART0_CR_UARTEN (1u << 0)
+#define UART0_CR_TXE_EN (1u << 8)
+#define UART0_LCR_H (*(volatile uint32_t *)(UART0_BASE + 0x02c))
+#define UART0_LCR_H_FEN (1u << 4)
+#define UART0_LCR_H_WLEN (3u << 5)
+
+#define TIMER_BASE 0x40054000u
+#define TIME_HR (*(volatile uint32_t *)(TIMER_BASE + 0x08))
+#define TIME_LR (*(volatile uint32_t *)(TIMER_BASE + 0x0c))
+
+uint64_t read_timer(void);
+void delay_ms(uint64_t milliseconds);
+
+void uart0_init(void);
+void uart0_putc(char);
+void uart0_puts(const char *);
+void uart0_putnum(uint64_t num);
+
+int main(void)
+{
+    uart0_init();
+
+    while (1)
+    {
+        uint64_t current_time = read_timer();
+        delay_ms(50);
+        uint64_t future_time = read_timer();
+
+        uint64_t diff = future_time - current_time;
+        uart0_puts("diff = ");
+        uart0_putnum(diff);
+        uart0_puts("\r\n");
+    }
+}
+
+void uart0_init(void)
+{
+    UART0_CR = 0;
+
+    GPIO0_CTRL = UART0_FUNC;
+
+    UART0_IBRD = 67;
+    UART0_FBRD = 52;
+
+    UART0_LCR_H = UART0_LCR_H_WLEN | UART0_LCR_H_FEN; // 8N1 8data, No parity, 1 stop bit, enable fifo
+
+    UART0_CR = UART0_CR_UARTEN | UART0_CR_TXE_EN;
+}
+
+void uart0_putc(char c)
+{
+    while ((UART0_FR & UART0_FR_TXFF) != 0)
+    {
+        // wait buffer full
+    }
+    UART0_DR = c;
+}
+
+void uart0_puts(const char *send)
+{
+
+    while (*send != '\0')
+    {
+        uart0_putc(*send);
+        send++;
+    }
+}
+
+void uart0_putnum(uint64_t num)
+{
+    if (num == 0)
+    {
+        uart0_putc('0');
+        return;
+    }
+    uint8_t buffer[20];
+    int index = 0;
+
+    while (num > 0)
+    {
+        buffer[index] = (num % 10) + '0';
+        num /= 10;
+        index++;
+    }
+
+    for (int i = index - 1; i >= 0; i--)
+    {
+        uart0_putc(buffer[i]);
+    }
+}
+
+uint64_t read_timer(void)
+{
+    uint32_t high1, low, high2;
+    do
+    {
+        high1 = TIME_HR;
+        low = TIME_LR;
+        high2 = TIME_HR;
+    } while (high1 != high2);
+
+    return ((uint64_t)high2 << 32) | low;
+}
+
+void delay_ms(uint64_t milliseconds)
+{
+    uint64_t start = read_timer();
+    uint64_t target_us = milliseconds * 1000;
+
+    while ((read_timer() - start) < target_us)
+    {
+        // wait;
+    }
+}
